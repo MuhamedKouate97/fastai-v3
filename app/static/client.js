64595e2bc6a1237c0d58a8b09bec9ae992e7c09b
MuhamedKouate97 <60383541+MuhamedKouate97@users.noreply.github.com>
2020-01-28 11:52:11 +0100

    Update client.js

--- app/static/client.js
+++ app/static/client.js
@@ -16,9 +16,9 @@ function showPicked(input) {
 
 function analyze() {
   var uploadFiles = el("file-input").files;
-  if (uploadFiles.length !== 1) alert("Please select a file to analyze!");
+  if (uploadFiles.length !== 1) alert("Inserisci un'immagine da analizzare!");
 
-  el("analyze-button").innerHTML = "Analyzing...";
+  el("analyze-button").innerHTML = "Analizzo...";
   var xhr = new XMLHttpRequest();
   var loc = window.location;
   xhr.open("POST", `${loc.protocol}//${loc.hostname}:${loc.port}/analyze`,
@@ -29,7 +29,7 @@ function analyze() {
   xhr.onload = function(e) {
     if (this.readyState === 4) {
       var response = JSON.parse(e.target.responseText);
-      el("result-label").innerHTML = `Result = ${response["result"]}`;
+      el("result-label").innerHTML = `Risultato = ${response["result"]}`;
     }
     el("analyze-button").innerHTML = "Analyze";
   };
